2632e5110cbd6988e76ce3685e0cd0d41a0e9536
Ahmad Bavarsad <42782509+ahmadbavarsad@users.noreply.github.com>
2023-09-26 14:47:44 +0330

    update models

--- letters/models.py
+++ letters/models.py
@@ -3,21 +3,17 @@ from django_jalali.db import models as jmodels
 from jalali_date.fields import JalaliDateField
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import FileExtensionValidator
-from django.utils.text import slugify
 
 
 #نامه وارده
 class IncomingLetter(models.Model):
     subject = models.CharField(max_length=100,verbose_name=_('عنوان نامه'))
-    slug = models.SlugField(unique=True, editable=False)
     document = models.FileField(upload_to='incoming_letters/',
     validators=[FileExtensionValidator(allowed_extensions=['pdf', 'jpg','jpeg'], message='پسوندهای مجاز pdf و jpg می باشد')], verbose_name=('تصویرنامه')
 )
-    def save(self, *args, **kwargs):
-        # Automatically generate the slug from the subject field
-        self.slug = slugify(self.subject)
-        super(IncomingLetter, self).save(*args, **kwargs)
-
+    IncomingDraft = models.FileField(upload_to='incoming_letters/draft',
+    validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx'], message='پسوندهای مجاز doc و docx می باشد')], verbose_name=('فایل ورد نامه')
+)
     def __str__(self):
         return self.subject
 
@@ -25,16 +21,11 @@ class IncomingLetter(models.Model):
 #نامه صادره
 class OutgoingLetter(models.Model):
     subject = models.CharField(max_length=100,verbose_name=_('عنوان نامه'))
-
     document = models.FileField(upload_to='outgoing_letters/',
     validators=[FileExtensionValidator(allowed_extensions=['pdf', 'jpg','jpeg'], message='پسوندهای مجاز pdf و jpg می باشد')], verbose_name=('تصویرنامه')
 )
-    slug = models.SlugField(unique=True, editable=False)
-
-    def save(self, *args, **kwargs):
-        # Automatically generate the slug from the subject field
-        self.slug = slugify(self.subject)
-        super(OutgoingLetter, self).save(*args, **kwargs)
-
+    OutgoingDraft = models.FileField(upload_to='outgoing_letters/draft',
+    validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx'], message='پسوندهای مجاز doc و docx می باشد')], verbose_name=('فایل ورد نامه')
+)
     def __str__(self):
         return self.subject
