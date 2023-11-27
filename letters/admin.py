cec3745da2f57983501730b85d1ae9d7aa807a11
Ahmad Bavarsad <42782509+ahmadbavarsad@users.noreply.github.com>
2023-11-27 11:22:41 +0330

    slug

--- letters/admin.py
+++ letters/admin.py
@@ -4,16 +4,24 @@ from jalali_date.widgets import AdminJalaliDateWidget, AdminSplitJalaliDateTime
 from .models import *
 from .forms import IncomingLetterAdminForm, OutgoingLetterAdminForm
 
+admin.sites.AdminSite.site_header = "پنل مدیریت نامه های کاردانی"
+    
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'slug')
+    prepopulated_fields = {"slug": ("title",)}  
 
+   
 @admin.register(IncomingLetter)
 class IncomingLetterAdmin(admin.ModelAdmin):
     form = IncomingLetterAdminForm
 
+
 @admin.register(OutgoingLetter)
 class OutgoingLetterAdmin(admin.ModelAdmin):
     form = OutgoingLetterAdminForm
-
+    
 
 
 admin.site.register(IncomingCategoryLetter)
 admin.site.register(OutgoingCategoryLetter)
+admin.site.register(Book, BookAdmin)
--- letters/models.py
+++ letters/models.py
@@ -3,24 +3,65 @@ from django_jalali.db import models as jmodels
 from jalali_date.fields import JalaliDateField
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import FileExtensionValidator
+from django.utils.text import slugify
+
+
+OPTIONS = (
+    ('COOL', 'COOL'),
+    ('WARM', 'WARM'),
+)
+class WriterName(models.Model):
+     options = models.CharField(max_length=20, choices=OPTIONS, default=None,blank=True, null=True)
+
+class Article(models.Model):
+    STATUS = Choices('draft', 'published')
+    status = models.CharField(choices=STATUS, default=STATUS.draft, max_length=20)
+
+
+class Book(models.Model):
+    title = models.CharField(db_column='title', max_length=100, blank=False)
+    writer_name = models.Choices(WriterName,)
+    description = models.TextField(db_column='description', max_length=1000, blank=False)
+    slug = models.SlugField(db_column='slug', blank=False, unique=True, allow_unicode=True,verbose_name='نام دلخواه')
+
+    class Meta:
+        db_table = 'book'
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+    def __unicode__(self):
+        return self.title
+    def __str__(self):
+        return self.title
+
 
 class IncomingCategoryLetter(models.Model):
     SahebanName = models.CharField(max_length=100, unique=True, verbose_name='صاحبان نامه ها')
 
+    class Meta:
+        db_table = 'IncomingCategoryLetter'
+        verbose_name = 'صاحبان نامه'
+        verbose_name_plural = 'صاحبان نامه'
+    def __unicode__(self):
+        return self.SahebanName
     def __str__(self):
         return self.SahebanName
-
+    
 #نامه وارده
 class IncomingLetter(models.Model):
     category = models.ManyToManyField(IncomingCategoryLetter, verbose_name='صاحبان نامه ها')
-    IncomingLetterNumber = models.CharField(max_length=20, verbose_name='شماره نامه')
     subject = models.CharField(max_length=50, verbose_name='عنوان نامه')
+    IncomingLetterNumber = models.CharField(max_length=20, verbose_name='شماره نامه')
     document = models.FileField(upload_to='incoming_letters/',
     validators=[FileExtensionValidator(allowed_extensions=['pdf', 'jpg','jpeg'], message='پسوندهای مجاز pdf و jpg می باشد')], verbose_name=('pdfیا jpgتصویرنامه')
 )
-    
+    class Meta:
+        db_table = 'IncomingLetter'
+        verbose_name = 'نامه وارده'
+        verbose_name_plural = 'نامه وارده'
+    def __unicode__(self):
+        return self.IncomingLetterNumber
     def __str__(self):
-        return self.subject
+        return self.IncomingLetterNumber
 
 class OutgoingCategoryLetter(models.Model):
     SahebanName = models.CharField(max_length=100, unique=True, verbose_name='صاحبان نامه ها')
@@ -34,11 +75,11 @@ class OutgoingLetter(models.Model):
     subject = models.CharField(max_length=50, verbose_name='عنوان نامه')
     OutgoingLetterNumber = models.CharField(max_length=20, verbose_name='شماره نامه')
     document = models.FileField(upload_to='outgoing_letters/', 
-    validators=[FileExtensionValidator(allowed_extensions=['pdf', 'jpg','jpeg'], message='پسوندهای مجاز pdf و jpg می باشد')], verbose_name=('تصویرنامه')
+    validators=[FileExtensionValidator(allowed_extensions=['pdf', 'jpg','jpeg'], message='پسوندهای مجاز pdf و jpg می باشد')], verbose_name=('تصویرنامه (pdf یا jpg)')
 ,blank=True)
     OutgoingDraft = models.FileField(upload_to='outgoing_letters/draft',
-    validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx'], message='پسوندهای مجاز doc و docx می باشد')], verbose_name=('فایل ورد نامه')
+    validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx'], message='پسوندهای مجاز doc و docx می باشد')], verbose_name=('فایل ورد نامه (doc یا docx)')
 )
     
     def __str__(self):
-        return self.subject
+        return self.OutgoingLetterNumber
